2df87d1f088a387cf0f3f1cb1c82de0af25d1505
Hrishit <hrishit.bp@gmail.com>
2022-02-28 20:44:16 +0530

    Page Layout Created

--- game.js
+++ game.js
@@ -1,5 +1,15 @@
-document.getElementById("Start").addEventListener("click",Play());
+var dice_value=0;
+
+document.getElementById('start').addEventListener("click",Play);
 function Play() {
     console.log("Working");
-    document.getElementById('game').style.display='block';
+    document.getElementById('heading').style.display='none';
+    document.getElementsByClassName('gameArea')[0].style.display='flex';
+    document.getElementById('GameFooter').style.display='flex';
+}
+document.getElementById("Dice_Roll").addEventListener('click',roll);
+function roll() {
+    console.log("Working");
+    dice_value=Math.ceil((Math.random()*6));
+    document.getElementById('dice').innerHTML=dice_value;
 }
